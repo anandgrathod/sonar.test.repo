e6c0f10f8329ac6c0b10d70fc0f3eb973a5a8925
anandgrathod <anandgrathod@gmail.com>
2023-09-15 05:37:36 +0530

    testcontaines changes

---
+++ src/main/java/com/example/sonar/test/repo/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.example.sonar.test.repo.repository;
+
+import com.example.sonar.test.repo.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
